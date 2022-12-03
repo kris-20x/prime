6807b9c0577ca50d29e5f7aec4846b407454c6f2
kris-20x <119753980+kris-20x@users.noreply.github.com>
2022-12-04 00:32:52 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,10 @@
+def prime(n):
+    for i in range(n+1):
+        if i>1:
+            for j in range(2,i):
+                if i%j==0:
+                    break
+            else:
+                print(i)
+        
+prime(100)
